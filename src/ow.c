1c5c006e92fd99b9151d158e68f95aaa9fe191ae
cctsao1008 <chiacheng.tsao@gmail.com>
2022-06-23 23:09:21 +0800

    Update ow.c
    Fix warning
    
    ..\src\ow.c(264): warning:  #186-D: pointless comparison of unsigned integer with zero

--- src/ow.c
+++ src/ow.c
@@ -261,7 +261,7 @@ static void owReadComplete(void) {
 		}
 		else {
 			owLastCommand = 0x00;
-			if (owBuf[1] >= 0 && owBuf[1] < NUM_RUN_MODES) {
+			if (owBuf[1] < NUM_RUN_MODES) {
 				runMode = owBuf[1];
 
 				owState = OW_WRITE;
